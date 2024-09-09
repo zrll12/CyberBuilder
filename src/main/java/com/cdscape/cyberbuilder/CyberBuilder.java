ec3fe879276d7df11b22bac5f83bab0ed1410c4a
zrll_ <46812903+zrll12@users.noreply.github.com>
2024-09-09 23:18:40 +0800

    dep: add world edit

--- src/main/java/com/cdscape/cyberbuilder/CyberBuilder.java
+++ src/main/java/com/cdscape/cyberbuilder/CyberBuilder.java
@@ -1,17 +1,33 @@
 package com.cdscape.cyberbuilder;
 
+import com.sk89q.worldedit.WorldEdit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class CyberBuilder extends JavaPlugin {
+    public static CyberBuilder INSTANCE;
+    public static Logger LOGGER;
+    public static boolean useWorldEdit = false;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        INSTANCE = this;
+        LOGGER = getLogger();
 
+        // Check if WorldEdit is installed
+        if (getServer().getPluginManager().getPlugin("WorldEdit") != null) {
+            useWorldEdit = true;
+            getLogger().info("CyberBuilder is running with WE " + WorldEdit.getVersion());
+        } else {
+            getLogger().warning("WorldEdit is not installed, some features will be disabled!");
+        }
+
+        getLogger().info("CyberBuilder has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("CyberBuilder has been disabled!");
     }
 }
